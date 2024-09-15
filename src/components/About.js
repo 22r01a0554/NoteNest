06cf98738d88c7c1f52719bfc93943a5ded46efd
22r01a0554 <148076637+22r01a0554@users.noreply.github.com>
2024-09-15 22:11:43 +0530

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,25 @@
+import React from 'react'
+const About = () => {
+  return (
+    <div>
+      This is About 
+    </div>
+  )
+}
+
+export default About
+
+
+// const About = () => {
+//   const a=useContext(noteContext)
+//   useEffect(() => {
+//     a.update();
+//     // eslint-disable-next-line 
+//   }, [])
+  
+//   return (
+//     <div>
+//       This is About {a.state.name} and he is in class {a.state.class}
+//     </div>
+//   )
+// }
